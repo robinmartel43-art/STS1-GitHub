1e9d4560862cac1905c1aa4cfd2636a60de6c3cf
baptisteclapeyron <baptiste.clapeyron@gmail.com>
2026-03-20 09:39:27 +0100

    Etudiant 1
    Fonction estMajeur et exposant ajoutée

--- TD_GitHub.cpp
+++ TD_GitHub.cpp
@@ -1,6 +1,19 @@
 //Fonctions réalisées par l'étudiant 1 :
+bool estMajeur(int age) {
+    if (age >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-
+int exposant(int nbre, int exp) {
+    int resultat = 1;
+    for (int i = 0; i < exp; i++) {
+        resultat = resultat * nbre;
+    }
+    return resultat;
+}
 
 
 
